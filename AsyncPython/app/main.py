539514cea1059730e85cc8d95c8e1467137b1fa3
Anton <an.vaseko@mail.ru>
2025-04-17 19:42:45 +0300

    3.1 Обработка HTTP-запросов

--- AsyncPython/app/main.py
+++ AsyncPython/app/main.py
@@ -1,23 +1,73 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
-from app.models import Feedback
+from app.models import UserCreate
 
 app = FastAPI()
 
-feedback_db = []
+sample_product_1 = {
+    "product_id": 123,
+    "name": "Smartphone",
+    "category": "Electronics",
+    "price": 599.99
+}
+
+sample_product_2 = {
+    "product_id": 456,
+    "name": "Phone Case",
+    "category": "Accessories",
+    "price": 19.99
+}
+
+sample_product_3 = {
+    "product_id": 789,
+    "name": "Iphone",
+    "category": "Electronics",
+    "price": 1299.99
+}
+
+sample_product_4 = {
+    "product_id": 101,
+    "name": "Headphones",
+    "category": "Accessories",
+    "price": 99.99
+}
+
+sample_product_5 = {
+    "product_id": 202,
+    "name": "Smartwatch",
+    "category": "Electronics",
+    "price": 299.99
+}
+
+products_db = [sample_product_1, sample_product_2, sample_product_3, sample_product_4, sample_product_5]
 
 
-@app.get('/messages')
-async def get_feedback():
-    return feedback_db
+@app.get('/product/{product_id}')
+async def get_product(product_id: int):
+    for product in products_db:
+        if product_id == product["product_id"]:
+            return product
+    raise ValueError("Товар в базе отсутствует")
 
 
 # Задача 2
-@app.post('/feedback')
-async def add_feedback(feedback: Feedback, is_premium: bool = False):
-    feedback.contact.is_premium = is_premium
-    feedback_db.append(feedback)
-    message = f"Спасибо, {feedback.name}! Ваш отзыв сохранён."
-    if is_premium:
-        message += " Ваш отзыв будет рассмотрен в приоритетном порядке."
-    return {"message": message}
+@app.get('/products/search')
+async def search_product(
+        keyword: str = Query(),
+        category: str = Query(default=None),
+        limit: int = Query(default=10)
+    ):
+    count = 0
+    response = []
+    for product in products_db:
+        if category:
+            if keyword in product["name"] and category == product["category"]:
+                response.append(product)
+                count += 1
+        else:
+            if keyword in product["name"]:
+                response.append(product)
+                count += 1
+        if count == limit:
+            break
+    return response
--- AsyncPython/app/models.py
+++ AsyncPython/app/models.py
@@ -1,31 +1,12 @@
-import re
-
 from typing import Annotated
-from pydantic import BaseModel, Field, AfterValidator, EmailStr
+from pydantic import BaseModel, Field, EmailStr
 
 
-def check_bad_words(value: str) -> str:
-    if re.search(r'\bредиск|\bбяк|\bкозявк', value.lower()):
-        raise ValueError('Использование недопустимых слов')
-    return value
-
-
-def check_phone(value: str) -> str:
-    if not re.search(r'^[0-9]{7,15}$', value):
-        raise ValueError('Ошибка в формате номера')
-    return value
-
-
-class Contact(BaseModel):
+class UserCreate(BaseModel):
+    name: str
     email: EmailStr
-    phone: Annotated[str, AfterValidator(check_phone)] = None
-    is_premium: bool = False
-
-
-class Feedback(BaseModel):
-    name: Annotated[str, Field(min_length=2, max_length=50)]
-    message: Annotated[str, Field(min_length=10, max_length=500), AfterValidator(check_bad_words)]
-    contact: Contact
+    age: Annotated[int, Field(gt=0)] = None
+    is_subscribed: bool = None
 
 
 if __name__ == "__main__":
